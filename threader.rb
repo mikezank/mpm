ae3c757aa9ed3600383eddd588bd7417dda31922
mike zanchettin <mike.zanchettin@gmail.com>
2016-05-16 20:27:54 -0500

    threader

---
+++ threader.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+# author: Oleg Sidorov <4pcbr> i4pcbr@gmail.com
+# this code is licenced under the MIT/X11 licence.
+
+require 'rubygems'
+require 'ffi-rzmq'
+
+thread1 = Thread.new{
+
+context = ZMQ::Context.new
+socket = context.socket(ZMQ::REP)
+socket.connect('tcp://localhost:5560')
+
+loop do
+  socket.recv_string(message = '')
+  puts "Received request: #{message}"
+  socket.send_string('World')
+end
+
+}
+
+thread2 = Thread.new{
+  
+  context = ZMQ::Context.new
+  frontend = context.socket(ZMQ::ROUTER)
+  backend = context.socket(ZMQ::DEALER)
+
+  frontend.bind('tcp://*:5559')
+  backend.bind('tcp://*:5560')
+
+  poller = ZMQ::Device.new(frontend, backend)
+}
+
+thread1.join
