e407ed145f26ddee9a9f15292d52b9acc9c1cf85
mike zanchettin <mike.zanchettin@gmail.com>
2016-06-04 12:47:39 -0500

    reverse

---
+++ hwrevclient.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'ffi-rzmq'
+
+def error_check(rc)
+    if ZMQ::Util.resultcode_ok?(rc)
+      false
+    else
+      STDERR.puts "Operation failed, errno [#{ZMQ::Util.errno}] description [#{ZMQ::Util.error_string}]"
+      caller(1).each { |callstack| STDERR.puts(callstack) }
+      true
+    end
+end
+
+context = ZMQ::Context.new(1)
+if ARGV[0] == 'local'
+  puts "Running client in local mode"
+  addr = "tcp://localhost:5560"
+else
+  addr = "tcp://45.79.199.41:5560"
+end
+
+# Socket to talk to server
+puts "Connecting to hello world server..."
+requester = context.socket(ZMQ::REP)
+requester.connect(addr)
+
+0.upto(9) do |request_nbr|
+  puts "Sending request #{request_nbr}..."
+  error_check(requester.send_string("Hello"))
+
+  reply = ''
+  error_check(requester.recv_string(reply))
+  
+  puts "Received reply #{request_nbr}: [#{reply}]"
+end
---
+++ hwrevserver.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+# author: Bill Desmarais bill@witsaid.com
+# this code is licenced under the MIT/X11 licence.
+
+require 'rubygems'
+require 'ffi-rzmq'
+
+def error_check(rc)
+    if ZMQ::Util.resultcode_ok?(rc)
+      false
+    else
+      STDERR.puts "Operation failed, errno [#{ZMQ::Util.errno}] description [#{ZMQ::Util.error_string}]"
+      caller(1).each { |callstack| STDERR.puts(callstack) }
+      true
+    end
+end
+
+context = ZMQ::Context.new(1)
+
+#puts "Starting Hello World server…"
+
+puts "Starting Hello World server..."
+# socket to listen for clients
+socket = context.socket(ZMQ::REQ)
+error_check(socket.bind("tcp://*:5560"))
+
+while true do
+  # Wait for next request from client
+  request = ''
+  error_check(socket.recv_string(request))
+
+  puts "Received request. Data: #{request.inspect}"
+
+  # Do some 'work'
+  sleep 1
+
+  # Send reply back to client
+  error_check(socket.send_string("world"))
+
+end
