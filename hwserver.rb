7d456217feb9e677ca1b5d0f6d38bf21b9ece743
mike zanchettin <mike.zanchettin@gmail.com>
2016-06-04 10:34:39 -0500

    connect *

--- hwserver.rb
+++ hwserver.rb
@@ -6,6 +6,16 @@
 require 'rubygems'
 require 'ffi-rzmq'
 
+def error_check(rc)
+    if ZMQ::Util.resultcode_ok?(rc)
+      false
+    else
+      STDERR.puts "Operation failed, errno [#{ZMQ::Util.errno}] description [#{ZMQ::Util.error_string}]"
+      caller(1).each { |callstack| STDERR.puts(callstack) }
+      true
+    end
+end
+
 context = ZMQ::Context.new(1)
 
 #puts "Starting Hello World server…"
@@ -13,12 +23,12 @@ context = ZMQ::Context.new(1)
 puts "Starting Hello World server..."
 # socket to listen for clients
 socket = context.socket(ZMQ::REP)
-socket.bind("tcp://localhost:5560")
+error_check(socket.bind("tcp://*:5560"))
 
 while true do
   # Wait for next request from client
   request = ''
-  rc = socket.recv_string(request)
+  error_check(socket.recv_string(request))
 
   puts "Received request. Data: #{request.inspect}"
 
@@ -26,6 +36,6 @@ while true do
   sleep 1
 
   # Send reply back to client
-  socket.send_string("world")
+  error_check(socket.send_string("world"))
 
 end
